6ce445d99c08ceecc1632cc43f7386e198855c7d
nehapramod <30772474+nehapramod@users.noreply.github.com>
2017-08-20 18:54:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,92 +1,275 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+var config = {
+    user: 'faizanbari1017',
+    database: 'faizanbari1017',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    passsword: process.env.DB_PASSWORD
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'some random value select',
+    cookie: {maxAge:1000*60*60*24*30}
+}));
 
-var articles ={
- 'article-one' :{
-    title:"Article one | Neha Pramod",
-    heading:'article one',
-    date:'Aug 10',
-    content:`<p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>
-             <p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>
-             <p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>
-             <p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>`
-},
- 'article-two' :{
-     title:"Article two | Neha Pramod",
-    heading:'article two',
-    date:'Aug 10',
-    content:`<p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>`
-             
- },
- 'article-three':{
-     title:"Article three | Neha Pramod",
-    heading:'article three',
-    date:'Aug 10',
-    content:`<p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>
-            
-             <p>
-                This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.This is the content.
-            </p>`
- }
-};
+
+
+
+
+
+
 function createTemplate(data){
     var title=data.title;
-    var heading=data.heading;
     var date=data.date;
+    var heading=data.heading;
     var content=data.content;
-    
-var htmlTemplate =`
-<html>
-    <head>
+
+    var htmlTemplate=`<html>
+        <head>
         <title>
             ${title}
         </title>
-        <meta name="viewport" content="width-devie-width,initial-scale=1"/>
+        <meta name="viewport" content="width = device-width, initial-scale = 1">
         <link href="/ui/style.css" rel="stylesheet" />
-       
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
+        </head>
+        <body>
+        <div class="container">    
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            
+            <h3>
+                ${heading} 
+            </h3>
+            
+            <div>
+                ${date.toDateString() }
+            </div>
+            
+            <div>
+                ${content}
+                    
+            </div>
         </div>
-        </hr>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-            ${content}
-        </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
+        </body>
+        
+    </html>
+    `;
+    return htmlTemplate;
 }
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(path.join(counter.toString()));
+});
+
+
+
+
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input, salt) {
+    
+    //how to create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000,512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+
+app.get('/hash/:input', function(req, res){
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+   });
+
+app.post('/create-user', function (req, res) {
+    //accepts username and password
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user1" (username,password) values ($1, $2)', [username, dbString], function (err, result){
+         if(err){
+            res.status(500).send(err.toString());
+        }else{
+            
+            res.send('user successfully created: ', + username); 
+        }
+  });
+});
+
+
+app.post('/login', function (req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * WHERE "user1" WHERE username = $1', [username], function (err, result){
+         if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length ===0 ) {
+                res.send(403).send('username/password is invalid');
+            }
+            else{
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+            var hashedPassword = hash(password, salt);
+            if (hashedPassword === dbString)
+            {
+                  req.session.auth = {userId: result.rows[0].id};
+                  res.send('credentials correct!!'); 
+            }
+           else
+           {
+               res.send(403).send('username/password is invalid');
+           }
+        
+            }
+             
+        }
+  });
+   });
+   
+app.get('/check-login',function (req, res) {
+    
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('you are logged in :' + req.session.auth.userId.toString());
+    }
+    else
+    {
+        res.send('you are not logged in');
+    }
+    
+    
+});
+ 
+ app.get('/logout',function(req, res){
+     delete req.session.auth;
+     res.send('logged out');
+     
+ });
+   
+   
+
+app.post('/create-user', function (req, res) {
+    //accepts username and password
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    
+  pool.query('SELECT * from INTO "user1" username =$1', [username], function (err, result){
+         if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length ===0)
+            {
+                res.send(403).send('username/password is invalid');
+            }
+            else{
+                var dbString = result.rows[0].password;
+                var salt = dbString
+                res.send('user successfully created: ', + username); 
+            }
+        }
+  });
+});
     
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+
+
+
+
+var pool = new Pool(config); 
+app.get('/test-db', function (req, res) {
+  //make a select request
+  //return a response with the results
+  pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            
+            res.send(JSON.stringify(result.rows)); 
+        }
+  
+  
+  });
+});
+
+
+
+
+
+
+
+var names=[];
+app.get('/submit_name/', function (req, res) {//URL:/submit_name?name=xxxx
+    
+    //Get the name from the request
+    
+    var name=req.query.name;
+    
+    names.push(name);
+    //JSON:Javascript object notation
+    res.send(JSON.stringify(names));
+});
+
+
+
+
+
+
+
+
+
+app.get('/articles/:articleName', function (req, res) {
+    
+  var articleName=req.params.articleName;    
+  
+  pool.query("SELECT * FROM sql1 WHERE title = $1",[req.params.articleName], function (err, result){
+        if (err)
+        {
+           res.status(500).send(err.toString()); 
+        }
+        else{
+            if (result.rows.length ===0){
+                res.status(404).send('Article not found');
+            }else{
+                var articleData = result.rows[0];
+                 res.send(createTemplate(articleData));
+            }
+        }
+  });
+  
 });
 
 
@@ -95,13 +278,26 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+var names=[];
+app.get('/submit_name/', function (req, res) {//URL:/submit_name?name=xxxx
+    
+    //Get the name from the request
+    
+    var name=req.query.name;
+    
+    names.push(name);
+    //JSON:Javascript object notation
+    res.send(JSON.stringify(names));
 });
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
