70a08fc3d91e52e720690c514acc4eb2140ca141
nehapramod <30772474+nehapramod@users.noreply.github.com>
2017-08-20 18:55:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,51 @@
-console.log('Loaded!');
+//submit username/password to login
+var nameInput=document.getElementById('name');
+//var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+     //create a request object
+    var request=new XMLHttpRequest();
+    
+    
+    //capture response in a variable
+    
+    request.onreadystatechange=function(){
+        
+        if(request.readyState==XMLHttpRequest.DONE){
+        //take some action
+    
+    
+        if(request.status==200){
+             console.log('userlogged in');
+             alert('logged in successfully');
+            
+        }
+         else if (request.status==403)
+         {
+             alert('username/password is incorrect');
+         }
+         else if (request.status==500)
+         {
+             alert('something went wrong on the server');
+         }
+        
+        } 
+        
+        //not done yet
+        
+        
+    };
+    
+    var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    //make the request
+    request.open('POST','http://faizanbari1017.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+    //Make a request to server and send name
+    
+    
+    };
+    
